0d3e807959f6d6c803665a08b576ea7d1b3fc225
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-21 05:08:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+   
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+   @roster[grade] = [student_name]
+   @roster.keys
+  end
+  
+  
+  # :roster[''} 
+  
+  
+end 
